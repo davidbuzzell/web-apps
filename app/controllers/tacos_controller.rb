c5f081874f33edd07a70b2918df2cc0c09454581
David Buzzell <david.buzzell@kellogg.northwestern.edu>
2026-02-12 02:01:25 +0000

    code along for tacos

---
+++ app/controllers/tacos_controller.rb
@@ -0,0 +1,6 @@
+class TacosController < ApplicationController
+  def index
+    # Looks for the file: app/views/<controller>/<action>.html.erb
+    #                     app/views/tacos/index.html.erb
+  end
+end
